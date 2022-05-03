1b3b2a6416340a8f5c19678b90de3f9c20c4e388
Xiaodong Li <bryne_lxd@sina.com>
2022-05-04 00:26:50 +0800

    completed hudi-start demo.

---
+++ hudi-learning/src/test/scala/com/xdorg1/hudi/test/HudiStartAppTest.scala
@@ -0,0 +1,10 @@
+package com.xdorg1.hudi.test
+
+object HudiStartAppTest {
+
+    def main(args: Array[String]): Unit = {
+        println("Hello World ..............")
+        println("I'm here, from Bryne.")
+    }
+
+}
