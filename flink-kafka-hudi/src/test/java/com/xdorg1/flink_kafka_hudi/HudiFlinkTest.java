ac9645b643108cc3da382baeceeb4cf7553a5eca
Xiaodong Li <bryne_lxd@sina.com>
2022-05-13 21:29:21 +0800

    add demo of kafka -> Flink connector read -> Flink connect write -> hudi -> Flink SQL read to show by using module of flinkkafkahudi.

---
+++ flink-kafka-hudi/src/test/java/com/xdorg1/flink_kafka_hudi/HudiFlinkTest.java
@@ -0,0 +1,9 @@
+package com.xdorg1.flink_kafka_hudi;
+
+public class HudiFlinkTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hudi Flink Kafka integration test ...................");
+    }
+
+}
