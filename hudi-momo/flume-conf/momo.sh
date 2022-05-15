53fbe2b246a108b03071b43bb5ff507b09763ea4
Xiaodong Li <bryne_lxd@sina.com>
2022-05-15 20:58:12 +0800

    Datagen and flume tools.

---
+++ hudi-momo/flume-conf/momo.sh
@@ -0,0 +1,5 @@
+java -jar /home/hadoop/hudi_demo/7mo_init/7Mo_DataGen.jar \
+/home/hadoop/hudi_demo/7mo_init/7Mo_Data.xlsx \
+/home/hadoop/hudi_demo/7mo_data \
+5000
+
---
+++ hudi-momo/flume-conf/start-flume.sh
@@ -0,0 +1,2 @@
+/usr/local/src/flume/bin/flume-ng agent -n a1  -c /usr/local/src/flume/conf -f /usr/local/src/flume/conf/7mo_mem_kafka.properties -Dflume.root.logger=INFO,console
+
